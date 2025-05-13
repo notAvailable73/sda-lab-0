4dcb60780d3cea47be03076b71cd79c6f52d8626
Your Name <you@example.com>
2025-05-13 12:37:18 +0600

    Add deployment code

--- server.js
+++ server.js
@@ -10,38 +10,47 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Database is connected..."))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Database is connected..."))
+    .catch((err) => console.log(err));
 
 //db schema
 const userSchema = mongoose.Schema({
-  name: String,
-  lastName: String,
+    name: String,
+    lastName: String,
 });
 
 //db model
 const User = new mongoose.model("User", userSchema);
 
 app.get("/get-users", (req, res) => {
-  User.find()
-    .then((users) => res.json(users))
-    .catch((err) => console.log(err));
+    User.find()
+        .then((users) => res.json(users))
+        .catch((err) => console.log(err));
 });
 
 app.post("/create", (req, res) => {
-  //save to mongodb and send response
-  const newUser = new User({
-    name: req.body.name,
-    lastName: req.body.lastName,
-  });
+    //save to mongodb and send response
+    const newUser = new User({
+        name: req.body.name,
+        lastName: req.body.lastName,
+    });
 
-  newUser
-    .save()
-    .then((user) => res.json(user))
-    .catch((err) => console.log(err));
+    newUser
+        .save()
+        .then((user) => res.json(user))
+        .catch((err) => console.log(err));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on post ${port}`);
+    console.log(`Server is running on post ${port}`);
 });
+
+
+const path = require('path')
+
+//production script
+app.use(express.static("./client/build"))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
