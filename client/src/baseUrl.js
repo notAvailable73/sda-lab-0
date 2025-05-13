3e086cf0858407511252f3ce9e2f17f5aec1c1db
Your Name <you@example.com>
2025-05-13 12:53:14 +0600

    change base url

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "http://localhost:3001";
+// const baseUrl = "http://localhost:3001";
+const baseUrl = "https://sda-lab01-dzfwffbee6c3ake7.eastasia-01.azurewebsites.net/";
 
 export default baseUrl;
